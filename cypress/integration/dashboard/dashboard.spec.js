83a4fdffe41319d8c31bf4f5603dec584f8b219a
ryanbuckman <ryanbuckman@outlook.com>
2022-01-11 13:26:10 +0000

    test: added dashboard tests

--- cypress/integration/dashboard/dashboard.spec.js
+++ cypress/integration/dashboard/dashboard.spec.js
@@ -7,9 +7,21 @@ describe('Dashboard', () => {
 		// Since we want to visit the same URL at the start of all our tests,
 		// we include it in our beforeEach function so that it runs before each test
 		cy.visit(' http://localhost:4200/');
+
+		cy.intercept('GET','api/heroes', {fixture: 'heroes.json'});
 	});
 
     it('Displays Top Heroes heading', () =>{
         cy.get('[data-cy=dashboard-heading]').should('have.text', 'Top Heroes');
     });
+
+	it('Displays the heroes menu', () =>{
+		cy.get('.heroes-menu > a').then(($links) =>{
+			expect($links).to.have.length(4);
+			expect($links[0]).to.contain.text('Narco').and.contain.attr('href');
+			expect($links[1]).to.contain.text('Bombasto').and.contain.attr('href');
+			expect($links[2]).to.contain.text('Celeritas').and.contain.attr('href');
+			expect($links[3]).to.contain.text('Magneta').and.contain.attr('href');		
+		});		
+	});
 });
